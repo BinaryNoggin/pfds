40fb6eb7d6f588da8c220485a0a48a60091f8a88
Amos L King <amos@binarynoggin.com>
2018-08-17 09:34:16 -0500

    Refactors queued tests
    There was some common setup that I thought should be pulled out.
    
    Amos King @adkron <amos@binarynoggin.com>

--- test/q_test.exs
+++ test/q_test.exs
@@ -4,45 +4,44 @@ defmodule Q.Test do
 
   property "q is a FIFO structure" do
     forall terms <- non_empty(list(integer())) do
-      subject = Q.empty()
-      queued = Enum.reduce terms, subject, fn term, sub ->
-        Q.snoc(sub, term)
-      end
-      {result, _sub} = Enum.reduce terms, {true, queued}, fn term, {result, sub} ->
-        cond do
-          Q.empty?(sub) ->
-            {result, sub}
-
-          true ->
+      with_queued_items(terms, fn queued ->
+        {result, _sub} =
+          Enum.reduce(terms, {true, queued}, fn term, {result, sub} ->
             {:ok, head} = Q.head(sub)
             {:ok, tail} = Q.tail(sub)
             {result && head == term, tail}
-        end
-      end
-      result
+          end)
+
+        result
+      end)
     end
   end
 
   property "q is empty only when there are no elements" do
     forall terms <- list(term()) do
-      subject = Q.empty()
-      queued = Enum.reduce terms, subject, fn term, sub ->
-        Q.snoc(sub, term)
-      end
-
-      Q.empty?(queued) == Enum.empty?(terms)
+      with_queued_items(terms, fn queued ->
+        Q.empty?(queued) == Enum.empty?(terms)
+      end)
     end
   end
 
   property "the tail of a non-empty q always has one less element than q" do
     forall terms <- non_empty(list(term())) do
-      subject = Q.empty()
-      queued = Enum.reduce terms, subject, fn term, sub ->
-        Q.snoc(sub, term)
-      end
-
-      {:ok, tail} = Q.tail(queued)
-      Q.length(tail) == length(terms) - 1
+      with_queued_items(terms, fn queued ->
+        {:ok, tail} = Q.tail(queued)
+        Q.length(tail) == length(terms) - 1
+      end)
     end
+  end
+
+  def with_queued_items(items, process) do
+    subject = Q.empty()
+
+    queued =
+      Enum.reduce(items, subject, fn term, sub ->
+        Q.snoc(sub, term)
+      end)
+
+    process.(queued)
   end
 end
